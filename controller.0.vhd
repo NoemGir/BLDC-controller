3738997872fb9c4bf9da0058502b78da8cf48a87
Noémie GIREAUD <noemie.gireaud@gmail.com>
2025-02-21 11:39:47 +0100

    input and output ports defined

--- controller.0.vhd
+++ controller.0.vhd
@@ -18,7 +18,6 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 use IEEE.std_logic_textio.all;
-use STD.textio.all;
 
 -- -----------------------------------------------------------------------------
 -- Definition de l'entite
@@ -34,25 +33,31 @@ entity memory is
 		MEM_SIZE : natural := 8;
 
 		-- front actif par defaut
-		ACTIVE_FRONT : std_logic := '1';
-
-		-- fichier d'initialisation
-		FILENAME : string := "" );
+		ACTIVE_FRONT : std_logic := '1'
+		
+		);
 
 	-- definition des entrees/sorties
 	port 	(
-		-- signaux de controle du cache
-		RST			: in std_logic; -- actifs a l'etat bas
-		CLK,RW		: in std_logic; -- R/W*
-		AS				: in std_logic; -- Address Strobe (sorte de /CS)
-
+		-- signaux de controle du controller
+		CLK      		: in std_logic; 
+		
+        EN				: in std_logic; 
+        RST				: in std_logic; 
+        
+        
 		-- bus d'adresse du cache
-		ADR			: in std_logic_vector(DBUS_WIDTH downto 0);
+		DUTY	    	: in std_logic_vector(DBUS_WIDTH downto 0);
 
-		-- Ports entree/sortie du cache
-		D				: in std_logic_vector(DBUS_WIDTH downto 0);
-		Q				: out std_logic_vector(DBUS_WIDTH downto 0) );
-
+		-- Ports sortie du controlleur 
+        U				: in std_logic; 
+        V				: in std_logic; 
+        W				: in std_logic; 
+        
+        Wn				: in std_logic; 
+        Vn				: in std_logic; 
+        Un				: in std_logic
+    );
 end memory;
 
 
@@ -61,13 +66,16 @@ end memory;
 -- -----------------------------------------------------------------------------
 architecture behavior of memory is
 
-	-- definition de constantes
-
 	-- definitions de types (index type default is integer)
-	type FILE_REGS is array (0 to DBUS_WIDTH) of std_logic_vector (DBUS_WIDTH-1 downto 0);
+	type FILE_REG_typ is array (0 to (2**DBUS_WIDTH)-1) of std_logic_vector (DBUS_WIDTH-1 downto 0);
 
-	-- declaration des ressources locales
-	signal REGS : FILE_REGS; -- le banc memoire
+	-- definition des ressources internes
+
+	-- registres
+	signal REGS : FILE_REG_typ; -- le banc de registres interne de la fifo
+
+	-- pointeurs lecture et ecriture
+	signal W_ADR,R_ADR	: std_logic_vector (DBUS_WIDTH-1 downto 0);
 
 begin
 --------------------------------------------
